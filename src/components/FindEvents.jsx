186a8683854c4d2de780794b5aae99a49feddf84
Timothy Allard <timallard@gmail.com>
2019-12-15 22:57:29 -0700

    ignore DS_Store files reverse order cleanup

--- src/components/FindEvents.jsx
+++ src/components/FindEvents.jsx
@@ -61,7 +61,7 @@ class FindEvents extends Component {
 				body =
 					<div>
 						<div className="row user-list mt-4">
-							{events_list.reverse()}
+							{events_list}
 						</div>
 						{pagination}
 					</div>
