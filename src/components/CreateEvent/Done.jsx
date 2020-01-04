4389fdae206c7e8daa0f707ecf0c4aa68b4a6ed6
Timothy Allard <timallard@gmail.com>
2020-01-04 14:40:06 -0700

    create event done message adjustments, state additions

--- src/components/CreateEvent/Done.jsx
+++ src/components/CreateEvent/Done.jsx
@@ -4,7 +4,7 @@ function Done(props) {
 	return (
 		<div className="mt-5 text-center">
 			<h3 className="mt-5">Done, your event has been created!</h3>
-			<p className="emoji"><span role="img" aria-label="sunglasses">😎</span></p>
+			<p className="emoji"><span role="img" aria-label="sunglasses">😎🎸</span></p>
 			<p>After transaction confirmation it will be available for users.</p>
 			<code>{props.message}</code>
 		</div>
