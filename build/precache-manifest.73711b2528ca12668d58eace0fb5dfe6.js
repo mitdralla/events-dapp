d28d97eba3d33b0ea3ba7d6be64970cb3516788f
Timothy Allard <timallard@gmail.com>
2020-01-26 23:00:33 -0700

    webpack testing

---
+++ build/precache-manifest.73711b2528ca12668d58eace0fb5dfe6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3e8ebf033b6c1f68ce1a652518b493",
+    "url": "/events-dapp/index.html"
+  },
+  {
+    "revision": "cfe2582d75fd7ca0428b",
+    "url": "/events-dapp/static/css/2.f502e26c.chunk.css"
+  },
+  {
+    "revision": "8e985bd1deacf81cfc0a",
+    "url": "/events-dapp/static/css/main.d289f3a1.chunk.css"
+  },
+  {
+    "revision": "cfe2582d75fd7ca0428b",
+    "url": "/events-dapp/static/js/2.bc15f880.chunk.js"
+  },
+  {
+    "revision": "fbd1e6ed071ee3c1a776bc7fbd5c6466",
+    "url": "/events-dapp/static/js/2.bc15f880.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e985bd1deacf81cfc0a",
+    "url": "/events-dapp/static/js/main.ad642836.chunk.js"
+  },
+  {
+    "revision": "b42097797a5ec0178d8e",
+    "url": "/events-dapp/static/js/runtime-main.6ea58195.js"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/events-dapp/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/events-dapp/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/events-dapp/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/events-dapp/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/events-dapp/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/events-dapp/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/events-dapp/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/events-dapp/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/events-dapp/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/events-dapp/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/events-dapp/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/events-dapp/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/events-dapp/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/events-dapp/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/events-dapp/static/media/fa-solid-900.f6121be5.woff2"
+  }
+]);
